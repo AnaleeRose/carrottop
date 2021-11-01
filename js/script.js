c9fe43e267e7c6153bb575cf9d6cabc88fd1682d
Ana <analeeskinner@gmail.com>
2021-10-31 20:32:40 -0500

    rewriting ajax call from another project for this one

---
+++ js/script.js
@@ -0,0 +1,49 @@
+function ajaxCall(fetch_url, body = false, testing = true) {
+	console.log("run ajax call")
+    if (body === true) {
+        body = new FormData(document.querySelector("#pollForm"));
+    }
+    fetch(fetch_url, {
+        method: 'POST',
+        headers: {
+            'Accept': 'text/html, application/json'
+        },
+        redirect: 'follow',
+        body: body,
+        // cors: "same-origin",
+        credentials: 'include'
+    }).then((response) => {
+            if (response.status == 404) {
+                console.error("dats no good, code 404")
+                data = false;
+            } else {
+                if (!testing) {
+                    data = response.json();
+                } else {
+                    data = response.text();
+                    console.log("testing: " + data)
+                }
+
+            }
+            return data;
+        }).then((data) => {
+                if (!data) {
+                    return;
+                }
+                if (!testing) {
+                    if (data['error']) {
+                        updatePoll(data, data['error']);
+                    } else {
+                        updatePoll(data);
+                    }
+                }
+            })
+}
+
+function updatePoll(data, errors = false) {
+    if (errors) {
+        console.error("Oh no: " + errors)
+    } else {
+        console.log("carry on")
+    }
+}
