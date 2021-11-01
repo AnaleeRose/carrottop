cec55734c329792e6d29c8df7e9a15c0abfb73b4
Ana <analeeskinner@gmail.com>
2021-10-31 22:01:18 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ voteBtns.forEach(function(element){
     element.addEventListener("click", function(e){
         e.preventDefault();
         let voteResponse = document.querySelectorAll('[name="vote_response"]')
-        voteResponse.value = e.getAttribute("data-vote-response");
+        voteResponse.value = element.getAttribute("data-vote-response");
         console.log(voteResponse)
     })
 });
